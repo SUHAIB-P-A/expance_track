c44b7d2011dde7a498196a27b134c3ebaacc7d57
SUHAIB <suhaibpa08@gmail.com>
2023-05-17 12:56:33 +0530

    register the database

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:expance_tracker/models/categories/catagory_model.dart';
 import 'package:expance_tracker/screens/sereen_home.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
-void main() {
+Future<void> main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(CategoryModelAdapter().typeId)) {
+    Hive.registerAdapter(CategoryModelAdapter());
+  }
+if (!Hive.isAdapterRegistered(catagorytypeAdapter().typeId)) {
+    Hive.registerAdapter(catagorytypeAdapter());
+  }
+
   runApp(const MyApp());
 }
 
