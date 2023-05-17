d07c802e6ecde5f0af606f424b2d33075b272e0a
SUHAIB <suhaibpa08@gmail.com>
2023-05-17 10:05:26 +0530

    add a tapbar on category page tabs are 'income'and 'expance'

--- lib/catagory_screen/catagory.dart
+++ lib/catagory_screen/catagory.dart
@@ -1,10 +1,49 @@
+import 'package:expance_tracker/catagory_screen/expance_cat_list.dart';
+import 'package:expance_tracker/catagory_screen/income_cat_list.dart';
 import 'package:flutter/material.dart';
 
-class Scr_catagory extends StatelessWidget {
+class Scr_catagory extends StatefulWidget {
   const Scr_catagory({super.key});
 
   @override
+  State<Scr_catagory> createState() => _Scr_catagoryState();
+}
+
+class _Scr_catagoryState extends State<Scr_catagory>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabcontroller1;
+
+  @override
+  void initState() {
+    _tabcontroller1 = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: const Text("catagory"));
+    return Column(
+      children: [
+        TabBar(
+          controller: _tabcontroller1,
+          tabs: const [
+            Tab(
+              text: 'incom',
+            ),
+            Tab(
+              text: 'expance',
+            ),
+          ],
+        ),
+        Expanded(
+          child: TabBarView(
+            controller: _tabcontroller1,
+            children: const [
+              Income(),
+              expance(),
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
