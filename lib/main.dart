681619a389cc29a08c8d3e982da63426db637f1c
SUHAIB <suhaibpa08@gmail.com>
2023-05-23 15:32:20 +0530

    register the transationAdapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:expance_tracker/models/categories/catagory_model.dart';
+import 'package:expance_tracker/models/transations/transation_model.dart';
 import 'package:expance_tracker/screens/sereen_home.dart';
 import 'package:expance_tracker/transation_screens/transation_add/transation_add_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
-Future<void> main()async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Hive.initFlutter();
   if (!Hive.isAdapterRegistered(CategoryModelAdapter().typeId)) {
     Hive.registerAdapter(CategoryModelAdapter());
   }
-if (!Hive.isAdapterRegistered(catagorytypeAdapter().typeId)) {
+  if (!Hive.isAdapterRegistered(catagorytypeAdapter().typeId)) {
     Hive.registerAdapter(catagorytypeAdapter());
+  }
+  if (!Hive.isAdapterRegistered(TransationModelAdapter().typeId)) {
+    Hive.registerAdapter(TransationModelAdapter());
   }
 
   runApp(const MyApp());
@@ -29,9 +33,9 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const Home(),
-      routes:{
-        screentransationadd.routename:(ctx) => const screentransationadd()
-      } ,
+      routes: {
+        screentransationadd.routename: (ctx) => const screentransationadd()
+      },
     );
   }
 }
